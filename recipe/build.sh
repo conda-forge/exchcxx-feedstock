e0c2e5ab0d7f31a904ea8721a0f4142b2317be2d
Sebastian Ehlert <sehlert@microsoft.com>
2026-01-02 12:14:55 +0100

    Enable cuda in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 set -ex
+if [[ ${cuda_compiler_version} != "None" ]]; then
+  CUDA=ON
+  CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=70;80 ${CMAKE_ARGS}"
+else
+  CUDA=OFF
+fi
 
-cmake -B _build -G Ninja -DBUILD_SHARED_LIBS=ON -DCMAKE_REQUIRE_FIND_PACKAGE_Libxc=ON ${CMAKE_ARGS}
-cmake --build _build
+cmake \
+  -B _build \
+  -G Ninja \
+  -DBUILD_SHARED_LIBS=ON \
+  -DEXCHCXX_ENABLE_CUDA=${CUDA} \
+  -DCMAKE_REQUIRE_FIND_PACKAGE_Libxc=ON \
+  ${CMAKE_ARGS}
+ninja -C _build -j1
 cmake --install _build
